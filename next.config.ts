0ea4216206e7dd2d2c1b62b1af73a82f3db83eb6
Tomas Jansson <tomas.jansson@gmail.com>
2025-01-15 16:18:26 +0100

    Add next config rewrite example

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/inspect2/:path*",
+        destination:
+          "https://request-inspector-nine.vercel.app/api/inspect/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
